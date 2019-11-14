0b329d5775712800fcf6bcb8a50b8e66298e4baa
gururajans <gururajan.s@outlook.com>
2019-11-14 07:10:11 -0800

    commit_api_1

--- api_test_1.py
+++ api_test_1.py
@@ -1,4 +1,5 @@
+#importing the requests library to support api fucntions
 import requests
 #url to receive teh response from
-response = requests.get("######")
-print(response.status_Code)
+response = requests.get("http://api.open-notify.org/this-api-doesnt-exist")
+print(response.status_code)
