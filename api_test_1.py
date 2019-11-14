525289bfdacb8e375e294b1606d13d1a2434aa8d
gururajans <gururajan.s@outlook.com>
2019-11-14 08:46:27 -0800

    200_and 404 updated info

--- api_test_1.py
+++ api_test_1.py
@@ -1,5 +1,9 @@
 #importing the requests library to support api fucntions
 import requests
-#url to receive teh response from
-response = requests.get("http://api.open-notify.org/this-api-doesnt-exist")
-print(response.status_code)
+#url to receive the response from notn working end-point
+#response = requests.get("http://api.open-notify.org/this-api-doesnt-exist")
+#success code 200 - with working end-point
+response2= requests.get("http://api.open-notify.org/astros.json")
+print(response2.status_code)
+#print the data recived as response as JSON
+print(response2.json())
